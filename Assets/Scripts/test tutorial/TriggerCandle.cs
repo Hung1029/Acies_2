f38643b64098fad27ad2a5bcfc63cf6ae8b9ecbc
Hung <psi94133@gmail.com>
2020-12-21 05:10:45 +0800

    update_ForestLight

--- Assets/Scripts/test tutorial/TriggerCandle.cs
+++ Assets/Scripts/test tutorial/TriggerCandle.cs
@@ -10,12 +10,31 @@ public class TriggerCandle : MonoBehaviour
     [System.NonSerialized]
     public bool _bSkillOneTrigger = false;
 
+    [System.NonSerialized]
+    public bool _bLightBlowEnable = true;
+
+    public GameObject Target;
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, 1.0f) / 1.0f;
+        if (_bLightBlowEnable == true)
+        {
+            Target.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().intensity += t;
+        }
+        else
+        {
+            Target.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().intensity = 1.5f;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "VitaSoul")
         {
             _bSkillOneTrigger = true;
+            _bLightBlowEnable = false;
 
         }
     }
